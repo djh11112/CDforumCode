6a1f6b2f341a5e3e280cb689d6b918e93502edd4
djh11112 <1282617328@qq.com>
2022-01-05 18:05:46 +0800

    111

--- cdforum-manManage/src/main/java/com/cdforum/manManage/manManageServer.java
+++ cdforum-manManage/src/main/java/com/cdforum/manManage/manManageServer.java
@@ -2,8 +2,10 @@ package com.cdforum.manManage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class manManageServer {
     public static void main(String[] args) {
         SpringApplication.run(manManageServer.class);
